bda0b5ebc4444c7c1d2d8d2575d59bfd5f08c7ec
JakelynnWalker <jakelynnwalker15@icloud.com>
2021-06-09 10:44:26 -0500

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello what is ur name!");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+            Console.WriteLine("What year is it?");
+
+            var year = int.Parse(Console.ReadLine())
+
+            Console.WriteLine($"Correct! The year is {year}");
+        }
+    }
+}
